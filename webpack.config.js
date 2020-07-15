840db996a3c495ce4ba7606ab9b91017321f971c
semperry <ryan.curtis88@gmail.com>
2020-07-15 16:10:18 -0600

    update webpack to fix browser router compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,40 @@
-var path = require("path");
+const path = require("path");
+
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "index.js",
-    libraryTarget: "commonjs2"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: path.resolve(__dirname, "src"),
-        exclude: /(node_modules|bower_components|build)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["env"]
-          }
-        }
-      }
-    ]
-  },
-  externals: {
-    react: "commonjs react"
-  }
+	mode: 'production',
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "lib"),
+		filename: "index.js",
+		libraryTarget: "commonjs2"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: 'babel-loader'
+			}
+		]
+	},
+	resolve: {
+		alias: {
+			'react': path.resolve(__dirname, './node_modules/react'),
+			'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+		}
+	},
+	externals: {
+		react: {
+			commonjs: 'react',
+			commonjs2: 'react',
+			amd: 'React',
+			root: 'React'
+		},
+		"react-dom": {
+			commonjs: "react-dom",
+			commonjs2: "react-dom",
+			amd: "ReactDOM",
+			root: "ReactDOM"
+		}
+	}
 };
